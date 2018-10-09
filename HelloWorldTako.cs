88857672ec2c2a47642e9095ff04c5a1b76ae3a6
takochako <32885729+takochako@users.noreply.github.com>
2018-10-09 18:18:50 -0400

    Add HelloWorldTako.cs

---
+++ HelloWorldTako.cs
@@ -0,0 +1,10 @@
+using System;
+namespace HelloWorldTako {
+    class HelloWorld {
+        static voice Main() {
+            Console.WriteLine("Hello world! Good luck on completing your 5 PRs!");
+
+            Console.ReadKey();
+        }
+    }
+}
